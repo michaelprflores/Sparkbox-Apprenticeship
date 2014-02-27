695f9203edf0d4d8b353591ad752d3a9512b66b2
michaelprflores <michelprflores@gmail.com>
2014-02-27 18:25:02 -0500

    Doing More Work

---
+++ Ruby_Training/horsin_with_Ruby/self_class.rb
@@ -0,0 +1,19 @@
+class Truck
+	def self.default_make
+		"mac"
+	end
+
+	def make1
+	self.class.default_make
+	end
+
+	def make2
+	Truck.default_make
+	end
+end
+
+class BigTruck < Truck
+	def self.default_make
+		"bigmac"
+	end
+end
