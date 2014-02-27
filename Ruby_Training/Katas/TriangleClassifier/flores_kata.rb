af5389e32e10918f2035935680b1accf64471506
michaelprflores <michelprflores@gmail.com>
2014-02-27 18:16:14 -0500

    Doing Work

--- Ruby_Training/Katas/TriangleClassifier/flores_kata.rb
+++ Ruby_Training/Katas/TriangleClassifier/flores_kata.rb
@@ -1,6 +1,16 @@
+# Ruby is a world of objects and how they relate to one another and hang and chill and make each other do stuff
+# Creating a Class
+# The problem:
+# We need to create a script or program that will tell me the kind of triangle I have with 3 pieces of data.
+# The 3 pieces of data are integers.
 class TriangleKata
+
+	# define a method with paramaters for the 3 integers as sides.
 	def kindoftriangle(sideA, sideB, sideC)
+
+		# write conditional statements for each triangle.
 		if sideA == 0 or sideB == 0 or sideC == 0
+			# returns is implied
 			"error"
 		elsif sideA == sideB and sideB == sideC
 			"equilateral"
@@ -12,11 +22,8 @@ class TriangleKata
 	end
 end
 
-my_triangle = TriangleKata.new
-my_result = my_triangle.kindoftriangle(1, 2, 3)
+# Test Driven Development again (per Mike Yockey)
 
-
-puts my_result == "scalene"
 puts TriangleKata.new.kindoftriangle(1, 2, 3) == "scalene"
 puts TriangleKata.new.kindoftriangle(3, 3, 3) == "equilateral"
 puts TriangleKata.new.kindoftriangle(2, 2, 3) == "isosceles"
@@ -24,4 +31,15 @@ puts TriangleKata.new.kindoftriangle(3, 2, 3) == "isosceles"
 puts TriangleKata.new.kindoftriangle(3, 2, 2) == "isosceles"
 puts TriangleKata.new.kindoftriangle(0, 2, 2) == "error"
 
-# Test Driven Development again (per Mike Yockey)
+# Another way to test by assigning variables.
+
+my_triangle = TriangleKata.new
+my_result = my_triangle.kindoftriangle(1, 2, 3)
+
+
+puts my_result == "scalene"
+
+# Use Git for everything
+# Separate Documents
+
+# Setting time to review together apprenticeship
