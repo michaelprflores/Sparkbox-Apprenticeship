57b9c8b98319128ca638e892ba2104776556cfe5
michaelprflores <michelprflores@gmail.com>
2014-03-13 18:19:02 -0400

    kept working on kata

--- Ruby_Training/Katas/Potter_Kata/potter.rb
+++ Ruby_Training/Katas/Potter_Kata/potter.rb
@@ -1,20 +1,23 @@
 class Potter
 
 	def cost(books)
+		array_difference(books)
 		total = 0
-		@unique_books = books.uniq
-		total = books.length * 8
+			if @ret.length >= 1
+				total += 8
+			end
 		return total
 	end
 
 	def array_difference(books)
-		cost(books)
-		ret = books.dup
+		@ret = books.dup
+		@unique_books = books.uniq
 		@unique_books.each do |element|
-		if index = ret.index(element)
-		ret.delete_at(index)
+		if index = @ret.index(element)
+		@ret.delete_at(index)
 		end
 	end
-		ret
+		return @ret
 	end
+
 end
