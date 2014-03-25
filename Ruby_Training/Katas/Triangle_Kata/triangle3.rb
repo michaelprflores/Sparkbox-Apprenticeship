da2943b7ddc3ce513084204868652962568ab73d
michaelprflores <michelprflores@gmail.com>
2014-03-25 16:33:47 -0400

    chore:did some work on triangle3.rb

--- Ruby_Training/Katas/Triangle_Kata/triangle3.rb
+++ Ruby_Training/Katas/Triangle_Kata/triangle3.rb
@@ -1,16 +1,16 @@
-class NameofTriangles
+class Triangle
 	def initialize(side1, side2, side3)
-		@scalene = scalene
-		@iscocles = iscocles
-		@equilateral = equilateral
+		@side1 = side1
+		@side2 = side2
+		@side3 = side3
 	end
 
-	def scalene
-	end
-
-	def isoscles
-	end
-	
-	def equilateral
+	def display
+		puts @side1
+		puts @side2
+		puts @side3
 	end
 end
+
+Triangle.new("1","2","3").display
+Triangle.new("4","5","6").display
