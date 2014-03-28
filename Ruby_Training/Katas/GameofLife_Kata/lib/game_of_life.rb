79642c3e7aa4aa2daf278b7d3177073b91e90d9d
michaelprflores <michelprflores@gmail.com>
2014-03-28 12:24:58 -0400

    work:did some work with Ryan

--- Ruby_Training/Katas/GameofLife_Kata/lib/game_of_life.rb
+++ Ruby_Training/Katas/GameofLife_Kata/lib/game_of_life.rb
@@ -1,11 +1,34 @@
-class Student
-	def initialize(name, grade, subject)
-		@name = [ ]
-		@grade = [ ]
-		@subject = [ ]
+class Wrangler
+
+	# New Story: School, number of students, name of the school, and an address to include city, state, zipcode.
+
+	def initialize
+	# 	@color = nil
+		@top = "hard"
+		@doors = 4
 	end
 
-	def name
-		puts @name
+	def color
+		@color 
+	end
+
+	def top
+		@top
+	end
+
+	def doors
+		@doors
+	end
+
+	def change_color(new_color)
+		@color = new_color
+	end
+
+	def change_top(new_top)
+		@top = new_top
+	end
+
+	def change_door(new_doors)
+		@doors = new_doors
 	end
 end
--- Ruby_Training/Katas/GameofLife_Kata/spec/game_of_life_spec.rb
+++ Ruby_Training/Katas/GameofLife_Kata/spec/game_of_life_spec.rb
@@ -1,19 +1,49 @@
 require 'rspec'
 require_relative '../lib/game_of_life' # game_of_life.rb
 
-
-
-describe Student do
-	it "exists" do
-		student = Student.new
-		# default values
-  end
-  it "has a name" do
-  	student = Student.new
-  	student.name.should eq(name)
-  	# name
+describe Wrangler do
+  it "has a default color of nil" do
+    jeep = Wrangler.new
+    jeep.color.should eq(nil)
   end
 
-  # grade level
-  # favorite subject
+  it "has a default top of hard" do
+    jeep = Wrangler.new
+    jeep.top.should eq("hard")
+  end
+
+  it "has a default doors to 4 doors" do
+    jeep = Wrangler.new
+    jeep.doors.should eq(4)
+  end
+
+  it "can have the color blue" do
+    jeep = Wrangler.new
+    jeep.change_color("blue")
+    jeep.color.should eq("blue")
+  end
+
+  it "can have a hard top" do
+    jeep = Wrangler.new
+    jeep.change_top("hard")
+    jeep.top.should eq("hard")
+  end
+
+  it "can have a soft top" do
+    jeep = Wrangler.new
+    jeep.change_top("soft")
+    jeep.top.should eq("soft")
+  end
+
+  it "can have 2 doors" do
+    jeep = Wrangler.new
+    jeep.change_door(2)
+    jeep.doors.should eq(2)
+  end
+
+   it "can have 4 doors" do
+    jeep = Wrangler.new
+    jeep.change_door(4)
+    jeep.doors.should eq(4)
+  end
 end
