3dca882de8b55e69914527debc727285bd8251da
michaelprflores <michelprflores@gmail.com>
2014-04-10 16:29:06 -0400

    work:worked on school domain. made edits to HTML

---
+++ Ruby_Training/Scratch/cromwell_scratch.rb
@@ -0,0 +1,24 @@
+# def my_method(argument)
+#   # do something interesting here with argument
+#   # do something else
+#   # do a 3rd thing
+# end
+
+# def ___(argument)
+#   # do something interesting here with argument
+# end
+
+# { |argument| # do something interesting here with argument }
+
+# [1,2,3].each &my_method
+# [1,2,3].each { |argument| 
+#     # do something 
+#     # do something else
+#     # do a 3rd thing
+# }
+
+a = "Hey "
+
+[1, 3, 7].each do |n|
+    puts a * n
+end
