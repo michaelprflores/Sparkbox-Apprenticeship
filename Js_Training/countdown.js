00903a160e8f2b173e065d2dca8517d4c9183421
michaelprflores <michelprflores@gmail.com>
2014-05-13 14:58:43 -0400

    work:More Js training and some Compass stuff

---
+++ Js_Training/countdown.js
@@ -0,0 +1,17 @@
+var num = 10;
+while(num>0){
+  if (num % 2 === 0)
+    console.log(num);
+
+  num--;
+}
+
+// ----- badlands.js -----
+var parkIsOpen = true;
+
+if (parkIsOpen){
+         onsole.log("Welcome to the Badlands National Park! Try to enjoy your stay.");
+} else {
+        console.log("Sorry, the Badlands are particularly bad today. We're closed!");
+}
+// ----- end -----
