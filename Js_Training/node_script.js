c91c1d355895a4ef6cef6c4d9a9596c6fa1d1125
michaelprflores <michelprflores@gmail.com>
2014-05-22 09:19:57 -0400

    work:wrote some javascript dude

--- Js_Training/node_script.js
+++ Js_Training/node_script.js
@@ -1,14 +1,16 @@
-var side1 = 5
-var side2 = 3
-var side3 = 1
+console.log('Hello World');
 
-for() {
-  if () {
-    console.log("Equilateral");    
-  }
-  else if () {
-    console.log("Isosceles");    
-  }
-  else if () {
-  console.log("Scalene");
-}
+// var side1 = 5
+// var side2 = 3
+// var side3 = 1
+
+// for() {
+//   if () {
+//     console.log("Equilateral");    
+//   }
+//   else if () {
+//     console.log("Isosceles");    
+//   }
+//   else if () {
+//   console.log("Scalene");
+// }
