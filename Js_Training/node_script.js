346673e702ee9bbbf13a1fcab148c88ffb6e5fda
michaelprflores <michelprflores@gmail.com>
2014-05-22 09:01:44 -0400

    work:apprentice page edits

---
+++ Js_Training/node_script.js
@@ -0,0 +1,14 @@
+var side1 = 5
+var side2 = 3
+var side3 = 1
+
+for() {
+  if () {
+    console.log("Equilateral");    
+  }
+  else if () {
+    console.log("Isosceles");    
+  }
+  else if () {
+  console.log("Scalene");
+}
