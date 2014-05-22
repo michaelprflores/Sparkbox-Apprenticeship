ba8f22c26c688216456be928de6ebbbac24e7957
michaelprflores <michelprflores@gmail.com>
2014-05-22 14:50:16 -0400

    work:triangle kata completed

--- Js_Training/node_script.js
+++ Js_Training/node_script.js
@@ -1,16 +1,23 @@
-console.log('Hello World');
+// console.log('Hello World');
 
-// var side1 = 5
-// var side2 = 3
-// var side3 = 1
+function myTriangle (side1, side2, side3) {
+  if (side1 === side2 && side2 === side3) {
+    return "Equilateral";    
+  }
+  else if (side1 === side2 || side1 === side3 || side2 === side3) {
+    return "Isosceles";    
+  }
+  else {
+    return "Scalene";
+  }
+}
 
-// for() {
-//   if () {
-//     console.log("Equilateral");    
-//   }
-//   else if () {
-//     console.log("Isosceles");    
-//   }
-//   else if () {
-//   console.log("Scalene");
-// }
+function myPrinter (side1,side2,side3) {
+    console.log(myTriangle (side1, side2, side3));
+}
+myPrinter (4, 5, 3); //scalene
+myPrinter (5, 5, 3); //isosceles
+myPrinter (3, 5, 3); //isosceles
+myPrinter (1, 1, 1); //Equilateral
+
+// List of triangles to loop over, loop over a collection of sides
